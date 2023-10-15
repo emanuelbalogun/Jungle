1383a8232ade741d1390ff5cb2fda1455922809b
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-10-15 13:33:41 -0500

    implement admin categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,12 +1,6 @@
 class Admin::DashboardController < ApplicationController
-   before_action :authenticate
+  http_basic_authenticate_with name: ENV["ADMIN_USER_NAME"], password: ENV["ADMIN_PASSWORD"] 
   def show
   end
 
-  private
-  def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['ADMIN_USER_NAME'] && password == ENV['ADMIN_PASSWORD']
-    end
-  end
 end
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,5 @@ module New
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-  end
-end
+ end
+end 
