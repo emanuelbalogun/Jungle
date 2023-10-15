a26c491128dfa8e3d86ffeb21b35fe0e4cca4986
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-10-15 16:46:11 -0500

    Complete user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+ 
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    
+    if user && user.authenticate(params[:password])
+      # SMaintin user's State in the application
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+    # redirect if the user cannot be authenticated.
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
 end
